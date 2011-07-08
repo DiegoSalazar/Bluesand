f98abd09f5d47fd72fd292b07bd2891fffbbbb2a
Diego Salazar <diego@greyrobot.com>
2011-07-08 00:48:42 +0000

    push it baby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluesand::Application.config.secret_token = 'ae7871c0f95ab077e08e64c35ccd8ca30f8e1f181f5374c2f1df0350d74d484495bb8304a55ee76b3f72f8266a1593bb7118e5dc5c05b9ece3eca9a3ba5b0c7a'
